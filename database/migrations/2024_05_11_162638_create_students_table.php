e3723b93105ec49dc0e90bac3e869f1dc4e6b2dd
SarfarazQadir <sarfarazqadir594@gmail.com>
2024-05-11 21:39:11 +0500

    Commit

---
+++ database/migrations/2024_05_11_162638_create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('Name',100); 
+            $table->string('Email',100); 
+            $table->integer('Phone'); 
+            $table->integer('Age'); 
+            $table->string('Semester',100); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
